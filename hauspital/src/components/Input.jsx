0211e898bd51acb8be2a8d481c6e96d47737b251
jiwon <ysjjw2003@gmail.com>
2024-11-23 02:31:13 +0900

    💄 UI: MainPage 수정

--- hauspital/src/components/Input.jsx
+++ hauspital/src/components/Input.jsx
@@ -1,11 +1,9 @@
 const Input = ({ placeholder, value, onChange }) => {
   return (
     <div className="flex w-[353px] bg-gray-200 px-5 py-2.5 rounded-[var(--Corner-Large,16px)] ">
-      <input
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
+      <input placeholder={placeholder} value={value} onChange={onChange} />
     </div>
   );
 };
+
+export default Input;
--- hauspital/src/pages/MainPage.jsx
+++ hauspital/src/pages/MainPage.jsx
@@ -1,15 +1,28 @@
 import NextButton from "../components/NextButton";
 import Header from "../components/Header";
 import BackButton from "../components/BackButton";
-import LoginButton from "../components/loginButton";
+import LoginButton from "../components/LoginButton";
 import Title from "../components/Title";
 import Input from "../components/Input";
+import logo from "../assets/logo.svg";
 
 const MainPage = () => {
   return (
     <div>
-      <Input placeholder="이름" />
+      <div className="flex text-8 ">
+        <p className="text-col2">병원</p>
+        <p className="text-gray-800">을 </p>
+        <p className="text-col1">내 집</p>
+        <p className="text-gray-800">으로</p>
+      </div>
+      <div className="flex justify-center p-10">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="flex justify-center">
+        <LoginButton text="로그인" />
+      </div>
     </div>
   );
 };
+
 export default MainPage;
