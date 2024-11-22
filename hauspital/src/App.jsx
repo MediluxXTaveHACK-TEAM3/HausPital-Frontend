89fff67547d99559a41c5359f38c99975b0ebbfb
yoonjin-C <cyy000123@gmail.com>
2024-11-23 05:30:17 +0900

    ✨ Feat: 라우팅 추가

--- hauspital/src/App.jsx
+++ hauspital/src/App.jsx
@@ -5,11 +5,18 @@ import {
   Navigate,
 } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import DoctorMainPage from "./pages/DoctorPage/DoctorMainPage";
+import DoctorMatchPage from "./pages/DoctorPage/DoctorMatchPage";
+import FontPage from "./pages/FontPage";
 function App() {
   return (
     <Router future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/doctor" element={<DoctorMainPage />} />
+        <Route path="/doctor-match" element={<DoctorMatchPage />} />
+
+        <Route path="/onboarding-1" element={<FontPage />} />
       </Routes>
     </Router>
   );
