b68c11123860832339b0ea95b47247f6f3700307
jiwon <ysjjw2003@gmail.com>
2024-11-23 00:10:22 +0900

    💄 UI: Button component figma대로 스타일 변경

--- hauspital/src/components/Button.jsx
+++ hauspital/src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({text, onClick})=>{
     return (
-        <div className="flex justify-center items-center py-2 bg-orange-50 w-auto rounded-lg">
+        <div className="flex justify-center items-center py-5 px-[23px] gap-2.5 bg-gray-600 text-white w-353 rounded-lg">
             <button onClick={onClick}>{text}</button>
         </div>
     );
--- hauspital/tailwind.config.js
+++ hauspital/tailwind.config.js
@@ -2,13 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
+    extend: {colors: {
       white: "#FFF",
       black: "#000000",
       orange: {
         50: "#FFFAE6",
-      },
+      },},
+    
     },
   },
   plugins: [],
