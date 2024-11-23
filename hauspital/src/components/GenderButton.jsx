13d5e55ad2d71b24ccd9ab2ae2f51a734eb4f413
yoonjin-C <cyy000123@gmail.com>
2024-11-23 11:23:37 +0900

    🐛 Fix: 클릭범위 수정

--- hauspital/src/components/GenderButton.jsx
+++ hauspital/src/components/GenderButton.jsx
@@ -4,14 +4,13 @@ const GenderButton = ({ text }) => {
   const nav = useNavigate();
 
   return (
-    <div className="flex w-[353px] h-[44px] justify-center items-center gap-2 rounded-[var(--Corner-Large,16px)] text-white font-bold bg-col3 px-6 py-2 border-none">
-      <button
-        onClick={() => {
-          nav("/address");
-        }}
-      >
-        {text}
-      </button>
+    <div
+      className="flex w-[353px] h-[44px] justify-center items-center gap-2 rounded-[var(--Corner-Large,16px)] text-white font-bold bg-col3 px-6 py-2 border-none"
+      onClick={() => {
+        nav("/address");
+      }}
+    >
+      <div>{text}</div>
     </div>
   );
 };
