cbb5c5037b99938bfbadd82ecf68408866dc2e49
yoonjin-C <cyy000123@gmail.com>
2024-11-23 10:51:17 +0900

    ✏️ Fix: 버튼 클릭범위 수정

--- hauspital/src/components/NextButton.jsx
+++ hauspital/src/components/NextButton.jsx
@@ -1,7 +1,10 @@
 const NextButton = ({ text, onClick, tag }) => {
   return (
-    <div className="flex justify-center items-center py-7 gap-2.5 bg-gray-600 text-[21px] font-semibold text-white w-[353px] h-[7px] rounded-lg hover:bg-gray-500">
-      <button onClick={onClick}>{text}</button>
+    <div
+      className="flex justify-center items-center py-7 gap-2.5 bg-gray-600 text-[21px] font-semibold text-white w-[353px] h-[7px] rounded-lg hover:bg-gray-500 cursor-pointer"
+      onClick={onClick}
+    >
+      <div>{text}</div>
     </div>
   );
 };
